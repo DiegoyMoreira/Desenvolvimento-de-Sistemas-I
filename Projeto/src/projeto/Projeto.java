2567b63c040854a7d2638298f22dfc6b86a48b79
Aluno <Aluno@C302-LAB04-003>
2022-05-06 15:14:40 -0300

    cod de padrões de projetos

--- Projeto/src/projeto/Projeto.java
+++ Projeto/src/projeto/Projeto.java
@@ -1,6 +1,19 @@
 package projeto;
 public class Projeto {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Fabrica fabrica = new Fabrica();
+        // obj fabrica //
+        Transporte obj1;
+        obj1 = fabrica.getFabrica(1);
+        // obj 1 //
+        Transporte obj2;
+        obj2 = fabrica.getFabrica(2);
+        // obj 2 //
         
+        obj1.entregar();
+        obj2.entregar();
+        obj1.abastecer();
+        obj2.abastecer();
+        // Metodos entregar e abastecer //
     }
 }
